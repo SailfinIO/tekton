0cf45cd7038dcfbe777592614b5bf2b19b0328b4
Michael Curtis <mpbcurtis@gmail.com>
2024-11-07 21:31:00 -0500

    fix: logger import

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,4 +1,4 @@
-import { ILogger } from '../interfaces';
+import { ILogger } from '../interfaces/Ilogger';
 import { LogLevel } from '../enums';
 import { ColorCodes, ResetCode } from './ColorCodes';
 
